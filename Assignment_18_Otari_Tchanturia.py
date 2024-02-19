1e46748655143461b33a594b2baa217c680ad39e
MrOtari <otiko.tchanturia@gmail.com>
2024-02-19 23:10:19 +0400

    Assignment 18 - Otari Tchanturia

--- Assignment_18_Otari_Tchanturia.py
+++ Assignment_18_Otari_Tchanturia.py
@@ -42,16 +42,20 @@ print(my_func(3))
 
 ############# Question 3
 
-def commission(func):
-    def wrapper(balance, tran_amount):
-        tran_amount += 1
-        if balance < tran_amount:
-            return "Not enough balance!"
-        else: 
-            return func(balance, tran_amount)
-    return wrapper
+def commission(value):
+    def inner(func):
+        def wrapper(balance, tran_amount):
+            tran_amount += value
+            if balance < tran_amount:
+                return "Not enough balance!"
+            else: 
+                return func(balance, tran_amount)
+        return wrapper
+    return inner
 
-@commission
+com = 4 # Commission amount
+
+@commission(com)
 def transaction(balance, tran_amount):
     balance -= tran_amount
     return f"Balance is {balance}."
